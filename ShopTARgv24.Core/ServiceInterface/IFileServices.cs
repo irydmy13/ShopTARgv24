bfa5e3228d276b215a1bb6db136ed500dbd96e0d
Gleb Dranitsyn <GlebDranitsyn@hotmail.com>
2025-09-30 16:35:45 +0300

    Добавьте файлы проекта.

---
+++ ShopTARgv24.Core/ServiceInterface/IFileServices.cs
@@ -0,0 +1,12 @@
+﻿using ShopTARgv24.Core.Domain;
+using ShopTARgv24.Core.Dto;
+
+namespace ShopTARgv24.Core.ServiceInterface
+{
+    public interface IFileServices
+    {
+        void FilesToApi(SpaceshipDto dto, Spaceship spaceship);
+        Task<FileToApi> RemoveImageFromApi(FileToApiDto dto);
+        Task<List<FileToApi>> RemoveImagesFromApi(FileToApiDto[] dtos);
+    }
+}
