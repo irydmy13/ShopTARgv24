35454d0b69ba5fcea27630d09cf8c3f0220465f5
Gleb Dranitsyn <GlebDranitsyn@hotmail.com>
2025-09-30 18:34:40 +0300

    kindergarten FINAL

---
+++ ShopTARgv24.Data/Migrations/20250930153247_initial migration.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShopTARgv24.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class initialmigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FileToApis",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExistingFilePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SpaceshipId = table.Column<Guid>(type: "uniqueidentifier", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FileToApis", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Kindergartens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    GroupName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ChildrenCount = table.Column<int>(type: "int", nullable: true),
+                    KindergartenName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TeacherName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Kindergartens", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Spaceships",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    TypeName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    BuiltDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    Crew = table.Column<int>(type: "int", nullable: true),
+                    EnginePower = table.Column<int>(type: "int", nullable: true),
+                    Passengers = table.Column<int>(type: "int", nullable: true),
+                    InnerVolume = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ModifiedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Spaceships", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FileToApis");
+
+            migrationBuilder.DropTable(
+                name: "Kindergartens");
+
+            migrationBuilder.DropTable(
+                name: "Spaceships");
+        }
+    }
+}
