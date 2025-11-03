e784ade8ad3f924d224ee7364c33cfe7be4d2df4
irydmy13 <kotiukir@gmail.com>
2025-11-03 11:00:38 +0200

    OpenWeatherServices and IOpenIWeatherServices

--- ShopTARgv24/Program.cs
+++ ShopTARgv24/Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using ShopTARgv24.ApplicationServices.Services;
 using ShopTARgv24.Core.ServiceInterface;
+using ShopTARgv24.ApplicationServices.Services;
 using ShopTARgv24.Data;
-
 namespace ShopTARgv24
 {
     public class Program
@@ -11,42 +10,41 @@ namespace ShopTARgv24
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            builder.Services.AddControllersWithViews();
-
-            builder.Services.AddScoped<ISpaceshipsServices, SpaceshipsServices>();
-            builder.Services.AddScoped<IFileServices, FileServices>();
-            builder.Services.AddScoped<IRealEstateServices, RealEstateServices>();
-            builder.Services.AddScoped<IWeatherForecastServices, WeatherForecastServices>();
+            //builder.Services.AddScoped< SpaceshipServices>();
 
             builder.Services.AddDbContext<ShopTARgv24Context>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddHttpClient<ICocktailService, CocktailService>(client =>
-            {
-                client.BaseAddress = new Uri("https://www.thecocktaildb.com/api/json/v1/1/");
-            });
-
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddScoped<ISpaceshipsServices, SpaceshipsServices>();
+            builder.Services.AddScoped<IFileServices, FileServices>();
+            builder.Services.AddScoped<IRealEstateServices, RealEstateServices>();
+            builder.Services.AddScoped<IWeatherForecastServices, WeatherForecastServices>();
+            // WebClient for ChuckNorrisServices
+            builder.Services.AddScoped<IChuckNorrisServices, ChuckNorrisServices>();
+            //HttpClient for ChuckNorrisServices
             builder.Services.AddHttpClient<IChuckNorrisServices, ChuckNorrisServices>();
-
- 
-            builder.Services.AddHttpClient<IOpenWeatherService, OpenWeatherService>((sp, client) =>
-            {
-                var cfg = sp.GetRequiredService<IConfiguration>();
-                var baseUrl = cfg["OpenWeather:BaseUrl"] ?? "https://api.openweathermap.org/data/2.5/";
-                client.BaseAddress = new Uri(baseUrl);
-            });
+            // HttpClient for CocktailServices
+            builder.Services.AddHttpClient<ICocktailService, CocktailService>();
+            // WebClient for OpenWeatherServices
+            builder.Services.AddScoped<IOpenWeatherServices, OpenWeatherServices>();
 
             var app = builder.Build();
 
+            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
             app.UseStaticFiles();
+
+            app.UseHttpsRedirection();
             app.UseRouting();
+
             app.UseAuthorization();
 
             app.MapStaticAssets();
